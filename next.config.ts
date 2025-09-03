04c0be884699c1888355b520e10d6e47d1e25b30
jhonlpjr <jhonlpjr@gmail.com>
2025-09-02 20:35:16 -0500

    Updated Next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
+
+// lee envs para decidir
+const useDockerRewrite = process.env.USE_DOCKER_REWRITE === "true";
+const internalApi = process.env.API_INTERNAL_URL; // ej: "http://api:8080"
+
 const nextConfig = {
-  output: 'export', // habilita `next export` para S3 estático
+  output: "export", // habilita `next export` para S3 estático
+
+  async rewrites() {
+    // ⚡ solo agrega rewrites en local con compose
+    if (useDockerRewrite && internalApi) {
+      return [
+        {
+          source: "/api/:path*",
+          destination: `${internalApi}/:path*`,
+        },
+      ];
+    }
+    return [];
+  },
 };
 
 export default nextConfig;
